9654fa86242c37ad01acb5e72beaf36cc3dd5899
Aaron Sapp <aaron.sapp@ebth.com>
2018-05-04 09:32:45 -0400

    updated labels/descriptions

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@ set -ex
 
 echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 
-IP=$(curl -F file=@${file_path_input} -F channels=${slack_channel_input} -F initial_comment="${slack_comment_input}" -F token=${slack_token_input} https://slack.com/api/files.upload)
+IP=$(curl -F file=@${file_path_input} -F channels=${slack_channel_input} -F title="${slack_title_input}" -F initial_comment="${slack_comment_input}" -F token=${slack_token_input} https://slack.com/api/files.upload)
 
 if [[ $IP = *"invalid_auth"* ]]; then
   exit 1
